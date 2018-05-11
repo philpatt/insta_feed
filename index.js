12b6225219335f7396fe6231d60ee21c51bce2f9
philpatt <phillip.s.patterson1@gmail.com>
2018-05-11 12:20:35 -0700

    update security

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 let express = require ('express');
 let path = require('path');
 let app = express();
+let ejsLayouts = require('express-ejs-layouts');
+
+
 
 app.set('view engine', 'ejs');
+app.use(ejsLayouts);
 
 
 app.get('/', function(req, res){
-    res.render('layout');
+    res.render('home');
 });
+
+
+app.use('/insta-feed', require('./controllers/insta-feed'));
+
 
 app.listen(3000);
